460803d021f5837ab440cdfe1f0d474007557b3c
Wijdentl <tlili.wijden@enis.tn>
2025-01-16 17:31:18 +0100

    Add project files.

---
+++ SecureAPI_JWT/Services/AuthService.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.Extensions.Options;
+using SecureAPI_JWT.Helpers;
+using SecureAPI_JWT.Models;
+
+namespace SecureAPI_JWT.Services
+{
+    public class AuthService : IAuthService
+    {
+        private readonly UserManager<ApplicationUser> _userManager;
+        private readonly RoleManager<IdentityRole> _roleManager;
+        private readonly JWT _jwt;
+
+        public AuthService(UserManager<ApplicationUser> userManager, RoleManager<IdentityRole> roleManager, IOptions<JWT> jwt)
+        {
+            _userManager = userManager;
+            _roleManager = roleManager;
+            _jwt = jwt.Value;
+        }
+        public async Task<AuthModel> RegisterAsync(RegisterModel model)
+        {
+            if (await _userManager.FindByEmailAsync(model.Email) is not null)
+            {
+                return new AuthModel { Message = "Email is already registered!" };
+            }
+
+            if (await _userManager.FindByNameAsync(model.Username) is not null)
+            {
+                return new AuthModel { Message = "Username is already registered!" };
+            }
+
+            var user = new ApplicationUser
+            {
+                UserName = model.Username,
+                Email = model.Email,
+                FirstName = model.FirstName,
+                LastName = model.LastName
+            };
+
+            var result = await _userManager.CreateAsync(user, model.Password);
+
+            if (!result.Succeeded)
+            {
+                var errors = string.Empty;
+                foreach (var error in result.Errors)
+                {
+                }
+}
+            //page 19
---
+++ SecureAPI_JWT/Services/IAuthService.cs
@@ -0,0 +1,9 @@
+﻿using SecureAPI_JWT.Models;
+
+namespace SecureAPI_JWT.Services
+{
+    public interface IAuthService
+    {
+        Task<AuthModel> RegisterAsync(RegisterModel model);
+    }
+}
